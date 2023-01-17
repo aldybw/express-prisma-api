2aece874b8c195c3928e49d3ca65628dd5bc8277
aldyckybw <bagusaldycky@gmail.com>
2023-01-17 11:41:01 +0700

    crud author

--- src/author/author.router.ts
+++ src/author/author.router.ts
@@ -15,3 +15,71 @@ authorRouter.get("/", async (request: Request, response: Response) => {
     return response.status(500).json(error.message);
   }
 });
+
+// GET: A single author by ID
+authorRouter.get("/:id", async (request: Request, response: Response) => {
+  const id: number = parseInt(request.params.id, 10);
+  try {
+    const author = await AuthorService.getAuthor(id);
+    if (author) {
+      return response.status(200).json(author);
+    }
+    return response.status(404).json("Author could not be found");
+  } catch (error: any) {
+    return response.status(500).json(error.message);
+  }
+});
+
+// POST: Create a Author
+// Params: firstName, lastName
+authorRouter.post(
+  "/",
+  body("firstName").isString(),
+  body("lastName").isString(),
+  async (request: Request, response: Response) => {
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+      return response.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const author = request.body;
+      const newAuthor = await AuthorService.createAuthor(author);
+      return response.status(201).json(newAuthor);
+    } catch (error: any) {
+      return response.status(500).json(error.message);
+    }
+  }
+);
+
+// PUT: Updating an Author
+// Params: firstName, lastName
+authorRouter.put(
+  "/:id",
+  body("firstName").isString().optional({ nullable: true }),
+  body("lastName").isString().optional({ nullable: true }),
+  async (request: Request, response: Response) => {
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+      return response.status(400).json({ errors: errors.array() });
+    }
+    const id: number = parseInt(request.params.id, 10);
+    try {
+      const author = request.body;
+      const updateAuthor = await AuthorService.updateAuthor(author, id);
+      return response.status(200).json(updateAuthor);
+    } catch (error: any) {
+      return response.status(500).json(error.message);
+    }
+  }
+);
+
+// DELETE: Delete an author based on the ID
+authorRouter.delete("/:id", async (request: Request, response: Response) => {
+  const id: number = parseInt(request.params.id, 10);
+  try {
+    await AuthorService.deleteAuthor(id);
+    return response.status(204).json("Author has been successfully deleted");
+  } catch (error: any) {
+    return response.status(500).json(error.message);
+  }
+});
--- src/author/author.service.ts
+++ src/author/author.service.ts
@@ -1,3 +1,4 @@
+import { transformDocument } from "@prisma/client/runtime";
 import { db } from "../utils/db.server";
 
 type Author = {
@@ -7,11 +8,63 @@ type Author = {
 };
 
 export const listAuthors = async (): Promise<Author[]> => {
-  return db.author.findMany({
+  return await db.author.findMany({
     select: {
       id: true,
       firstName: true,
       lastName: true,
     },
+  });
+};
+
+export const getAuthor = async (id: number): Promise<Author | null> => {
+  return await db.author.findUnique({
+    where: {
+      id,
+    },
+  });
+};
+
+export const createAuthor = async (
+  author: Omit<Author, "id">
+): Promise<Author> => {
+  const { firstName, lastName } = author;
+  return await db.author.create({
+    data: {
+      firstName,
+      lastName,
+    },
+    select: {
+      id: true,
+      firstName: true,
+      lastName: true,
+    },
+  });
+};
+
+export const updateAuthor = async (
+  author: Omit<Partial<Author>, "id">,
+  id: number
+): Promise<Author> => {
+  const { firstName, lastName } = author;
+  return db.author.update({
+    where: {
+      id,
+    },
+    data: {
+      firstName: firstName || undefined,
+      lastName: lastName || undefined,
+    },
+    select: {
+      id: true,
+      firstName: true,
+      lastName: true,
+    },
+  });
+};
+
+export const deleteAuthor = async (id: number): Promise<void> => {
+  await db.author.delete({
+    where: { id },
   });
 };
